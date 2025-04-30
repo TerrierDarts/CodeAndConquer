3c42487064251a4a23f16171959daa69a7ffe9c5
TerrierDarts <82717017+TerrierDarts@users.noreply.github.com>
2025-04-30 19:04:36 +0100

    Resources Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 
 export default defineConfig({
     integrations: [mdx()],
+    output: 'server',
+    adapter: cloudflare(),
 });
+
